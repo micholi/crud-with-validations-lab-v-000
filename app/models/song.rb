6749019aafde175cf30a15635f1adfc81abb882b
Michelle Olivieri <michelleo72@gmail.com>
2018-06-04 21:22:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: [:release_year, :artist_name]}
   validates :released, inclusion: {in: [true, false]}
   validates :artist_name, presence: true
-  if :released == true
-  validates :release_year, presence: true
+  validates :release_year, presence: true unless :released == false
   validates :release_year, numericality: {only_integer: true, less_than_or_equal_to: Time.now.year}
 end
 end
