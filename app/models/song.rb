89575473e9041d55760cdfec340f22186ab88837
Michelle Olivieri <michelleo72@gmail.com>
2018-06-04 20:28:16 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true if :released,
+  validates :artist_name, presence: true
 end
