b2057ab292349a1f65f11ab96a7b7c253e0f972e
Michelle Olivieri <michelleo72@gmail.com>
2018-06-04 20:45:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,11 @@
 class SongsController < ApplicationController
   
   def index
-
+    @songs = Song.all
   end
 
   def show
-
+    set_show
   end
 
   def new
@@ -17,8 +17,16 @@ class SongsController < ApplicationController
   end
 
   def edit
+    set_show
+  end
 
     def update
+      set_show
+    end
 
+    private
+
+    def set_song
+      @song = Song.find(params[:id])
     end
 end
